eaa778f177a97305ae2a2718f93418a5af3ea3b7
tng <m.farm4nov@gmail.com>
2024-02-05 23:31:01 +0400

    Removed unused pages

--- MarketplaceStock/Pages/Index.cshtml.cs
+++ MarketplaceStock/Pages/Index.cshtml.cs
@@ -1,19 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MarketplaceStock.Pages;
-
-public class IndexModel : PageModel
+namespace MarketplaceStock.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-
+        public void OnGet()
+        {
+            // Response.Redirect("/Authorization");
+        }
     }
 }
