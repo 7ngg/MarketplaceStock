8e032cd75adcb38a497b40058228e986e54d75c1
tng <m.farm4nov@gmail.com>
2024-02-12 15:19:36 +0400

    Admin users table

--- MarketplaceStock/Areas/Admin/Controllers/AdminController.cs
+++ MarketplaceStock/Areas/Admin/Controllers/AdminController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using StockDataLayer.Contexts;
 
 namespace MarketplaceStock.Areas.Admin.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly MarketplaceStockContext _context;
+
+        [ActivatorUtilitiesConstructor]
+        public AdminController(MarketplaceStockContext context)
+        {
+            _context = context;
+        }
+
         [Area("Admin")]
         public IActionResult Index()
         {
-            return View("~/Areas/Admin/Views/Index.cshtml");
+            var users = _context.Users.ToList();
+            return View("~/Areas/Admin/Views/Index.cshtml", users);
         }
     }
 }
--- MarketplaceStock/wwwroot/js/site.js
+++ MarketplaceStock/wwwroot/js/site.js
@@ -7,3 +7,12 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+function openForm() {
+    document.getElementById("popup-window").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("popup-window").style.display = "none"
+}
+
